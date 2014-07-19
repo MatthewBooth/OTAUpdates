32718a293d6fbc9d04419220d5cfff1e8daaee6c
Kryten2k35 <kryten2k35@gmail.com>
2014-07-19 17:06:41 +0100

    Fixed Makefile for AOSP building

--- Android.mk
+++ Android.mk
@@ -1,23 +1,21 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_STATIC_JAVA_LIBRARIES := android-support-v4 RootTools
+LOCAL_SRC_FILES := $(call all-subdir-java-files) $(call all-renderscript-files-under, src)
+
+LOCAL_STATIC_JAVA_LIBRARIES := \
+	RootTools \
+	android-support-v4
 
 LOCAL_PACKAGE_NAME := OTAUpdates
-
-LOCAL_SRC_FILES := $(call all-java-files-under,src)
-
-LOCAL_PROGUARD_FLAG_FILES := proguard.cfg
-
-LOCAL_AAPT_INCLUDE_ALL_RESOURCES := true
-
-LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := \
-	libs/android-support-v4.jar \
-	libs/RootTools.jar
-
-LOCAL_AAPT_INCLUDE_ALL_RESOURCES := true
+LOCAL_CERTIFICATE := shared
 
 include $(BUILD_PACKAGE)
 
+include $(CLEAR_VARS)
+
+LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := \
+libs/RootTools.jar \
+libs/android-support-v4.jar
+
 include $(BUILD_MULTI_PREBUILT)
-##
