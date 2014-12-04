e6ba909bfc826ae627fbcfd0c4105db973776551
Kryten2k35 <Kryten2k35@gmail.com>
2014-12-04 23:56:55 +0000

    Fix makefile for cardview

--- Android.mk
+++ Android.mk
@@ -3,15 +3,19 @@ include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := $(call all-subdir-java-files) $(call all-renderscript-files-under, src)
 
+cards_res := ../../../frameworks/support/v7/cardview/res
+res_dirs := $(cards_res) res
+
+LOCAL_PACKAGE_NAME := OTAUpdates
+LOCAL_CERTIFICATE := shared
+
 LOCAL_STATIC_JAVA_LIBRARIES := \
 	RootTools \
 	android-support-v4 \
 	android-support-v7-cardview
 
-LOCAL_PACKAGE_NAME := OTAUpdates
-LOCAL_CERTIFICATE := shared
-
-LOCAL_REQUIRED_MODULES := android-support-v7-cardview-res
+LOCAL_RESOURCE_DIR := $(addprefix $(LOCAL_PATH)/, $(res_dirs))
+LOCAL_AAPT_FLAGS := --auto-add-overlay
 
 include $(BUILD_PACKAGE)
 
