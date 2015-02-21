08d13cecc2ce63e51799842c76f7cf987ac3e248
Kryten2k35 <Kryten2k35@gmail.com>
2015-02-21 22:03:15 +0000

    About: Added BitCoin payments to the about page for the developer of the app (Me) on the rare occasion someone does that.

--- src/com/ota/updates/activities/AboutActivity.java
+++ src/com/ota/updates/activities/AboutActivity.java
@@ -18,10 +18,14 @@ package com.ota.updates.activities;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.text.Html;
 import android.view.View;
@@ -29,61 +33,90 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toolbar;
+
 import com.ota.updates.R;
 import com.ota.updates.utils.Preferences;
 import com.ota.updates.utils.Utils;
 
-public class AboutActivity extends Activity implements OnClickListener{
+public class AboutActivity extends Activity {
 	@SuppressLint("NewApi") @Override
 	protected void onCreate(Bundle savedInstanceState) {
 		Context context = this;
 		setTheme(Preferences.getTheme(context));
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.ota_about);
-		
+
 
 		if(Utils.isLollipop()){
 			Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_about);
 			setActionBar(toolbar);
 			toolbar.setTitle(getResources().getString(R.string.app_name));
 		}
-		
+
 		Typeface typeFace = Typeface.createFromAsset(getAssets(),"fonts/Roboto-Light.ttf");
-		
+
 		TextView aboutTitle = (TextView) findViewById(R.id.about_tv_about_title);
 		TextView donateTitle = (TextView) findViewById(R.id.about_tv_donate_title);
 		TextView creditsTitle = (TextView) findViewById(R.id.about_tv_credits_title);
 		TextView creditsSummary = (TextView) findViewById(R.id.about_tv_credits_summary);
 		Button donateButton = (Button) findViewById(R.id.about_btn_donate);
-		
+
 		aboutTitle.setTypeface(typeFace);
 		donateTitle.setTypeface(typeFace);
 		creditsTitle.setTypeface(typeFace);
-		
+
 		String openHTML = "";
 		if(Utils.isLollipop()){
 			openHTML = "<font color='#009688'>";
 		} else {
 			openHTML = "<font color='#33b5e5'>";
 		}
-        String closeHTML = "</font>";
-        String newLine = "<br />";
-        String creditsText =
-                openHTML + "Matt Booth" + closeHTML + " - Anything not mentioned below" + newLine +
-                openHTML + "Roman Nurik" + closeHTML + " - Android Asset Studio Framework" + newLine +
-                openHTML + "Jeff Gilfelt"+ closeHTML + " - Android Action Bar Style Generator" + newLine + 
-                openHTML + "Ficeto (AllianceROM)" + closeHTML + " - Shell tools" + newLine +
-                openHTML + "StackOverflow" + closeHTML + " - Many, many people";
-        creditsSummary.setText(Html.fromHtml(creditsText));
-        
-        donateButton.setOnClickListener(this);
+		String closeHTML = "</font>";
+		String newLine = "<br />";
+		String creditsText =
+				openHTML + "Matt Booth" + closeHTML + " - Anything not mentioned below" + newLine +
+				openHTML + "Roman Nurik" + closeHTML + " - Android Asset Studio Framework" + newLine +
+				openHTML + "Jeff Gilfelt"+ closeHTML + " - Android Action Bar Style Generator" + newLine + 
+				openHTML + "Ficeto (AllianceROM)" + closeHTML + " - Shell tools" + newLine +
+				openHTML + "StackOverflow" + closeHTML + " - Many, many people";
+		creditsSummary.setText(Html.fromHtml(creditsText));
+
+		donateButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				setupDonateDialog();
+			}
+		});
 	}
 
-	@Override
-	public void onClick(View v) {		
-		String url = "http://goo.gl/ZKSY4";
-		Intent intent = new Intent(Intent.ACTION_VIEW);
-		intent.setData(Uri.parse(url));
-		startActivity(intent);
+	private void setupDonateDialog() {
+		String[] items = { "PayPal", "BitCoin" };
+		Builder dialog = new AlertDialog.Builder(this);
+		dialog.setTitle("")
+		.setSingleChoiceItems(items, 0, null)
+		.setPositiveButton(getResources().getString(R.string.ok), new DialogInterface.OnClickListener() {
+
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				String url = "";
+				if (which == 0) {
+					url = "http://goo.gl/ZKSY4";
+				} else {
+					url = "http://goo.gl/o4c6ES";
+				}
+				Intent intent = new Intent(Intent.ACTION_VIEW);
+				intent.setData(Uri.parse(url));
+				startActivity(intent);
+			}
+		})
+		.setNegativeButton(getResources().getString(R.string.cancel), new DialogInterface.OnClickListener() {
+
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				dialog.cancel();
+			}
+		})
+		.show();		
 	}
 }
