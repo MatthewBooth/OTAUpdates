a4b9efdf499e32317033fd9a33524923e30604e9
MatthewBooth <kryten2k35@ultimarom.com>
2015-08-14 13:41:29 +0100

    Added Version information to the about page.
    This closes #63

--- app/src/main/java/com/ota/updates/fragments/AboutFragment.java
+++ app/src/main/java/com/ota/updates/fragments/AboutFragment.java
@@ -7,66 +7,42 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.ota.updates.R;
 import com.ota.updates.utils.FragmentInteractionListener;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link AboutFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link AboutFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class AboutFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     private FragmentInteractionListener mListener;
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AboutFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AboutFragment newInstance(String param1, String param2) {
-        AboutFragment fragment = new AboutFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     public AboutFragment() {
         // Required empty public constructor
     }
 
+    public static AboutFragment newInstance(String param1, String param2) {
+        return new AboutFragment();
+    }
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about, container, false);
+        View view = inflater.inflate(R.layout.fragment_about, container, false);
+
+        setupAppVersion(view);
+
+        return view;
+    }
+
+    private void setupAppVersion(View view) {
+        TextView tv = (TextView) view.findViewById(R.id.app_version);
+        String text = getResources().getString(R.string.about_version_number) + " " + getResources().getString(R.string.app_version);
+        tv.setText(text);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
