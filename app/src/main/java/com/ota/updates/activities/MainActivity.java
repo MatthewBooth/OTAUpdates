a0a9f2d911da94a8ca9e35f1fe54b021f614dfaf
MatthewBooth <kryten2k35@gmail.com>
2016-03-27 20:26:02 +0100

    Remove final bit of Google indexing code. Maybe later, Google.

--- app/src/main/java/com/ota/updates/activities/MainActivity.java
+++ app/src/main/java/com/ota/updates/activities/MainActivity.java
@@ -83,12 +83,6 @@ public class MainActivity extends AppCompatActivity implements App, FragmentInte
      */
     private Map<String, Object> mStartDownload = new HashMap<>();
 
-//    /**
-//     * ATTENTION: This was auto-generated to implement the App Indexing API.
-//     * See https://g.co/AppIndexing/AndroidStudio for more information.
-//     */
-//    private GoogleApiClient client;
-
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -127,10 +121,6 @@ public class MainActivity extends AppCompatActivity implements App, FragmentInte
 
         // Initializing Drawer Layout and ActionBarToggle
         initialisingDrawerLayout(mToolbar, navigationView);
-
-//        // ATTENTION: This was auto-generated to implement the App Indexing API.
-//        // See https://g.co/AppIndexing/AndroidStudio for more information.
-//        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     @Override
@@ -602,45 +592,5 @@ public class MainActivity extends AppCompatActivity implements App, FragmentInte
     @Override
     public void stopDownload(int fileId) {
         mFileDownload.removeDownload(fileId);
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-
-//        // ATTENTION: This was auto-generated to implement the App Indexing API.
-//        // See https://g.co/AppIndexing/AndroidStudio for more information.
-//        client.connect();
-//        Action viewAction = Action.newAction(
-//                Action.TYPE_VIEW, // TODO: choose an action type.
-//                "Main Page", // TODO: Define a title for the content shown.
-//                // TODO: If you have web page content that matches this app activity's content,
-//                // make sure this auto-generated web page URL is correct.
-//                // Otherwise, set the URL to null.
-//                Uri.parse("http://host/path"),
-//                // TODO: Make sure this auto-generated app deep link URI is correct.
-//                Uri.parse("android-app://com.ota.updates.activities/http/host/path")
-//        );
-//        AppIndex.AppIndexApi.start(client, viewAction);
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-
-//        // ATTENTION: This was auto-generated to implement the App Indexing API.
-//        // See https://g.co/AppIndexing/AndroidStudio for more information.
-//        Action viewAction = Action.newAction(
-//                Action.TYPE_VIEW, // TODO: choose an action type.
-//                "Main Page", // TODO: Define a title for the content shown.
-//                // TODO: If you have web page content that matches this app activity's content,
-//                // make sure this auto-generated web page URL is correct.
-//                // Otherwise, set the URL to null.
-//                Uri.parse("http://host/path"),
-//                // TODO: Make sure this auto-generated app deep link URI is correct.
-//                Uri.parse("android-app://com.ota.updates.activities/http/host/path")
-//        );
-//        AppIndex.AppIndexApi.end(client, viewAction);
-//        client.disconnect();
     }
 }
